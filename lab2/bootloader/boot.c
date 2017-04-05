058cb25c77b98e1a6d900e1e6d14e90845b06d76
luyiming <luyimingchn@gmail.com>
2017-04-05 22:04:32 +0800

    modify comments

--- lab2/bootloader/boot.c
+++ lab2/bootloader/boot.c
@@ -3,21 +3,16 @@
 #define SECTSIZE 512
 
 /*
-void asm_print(int row, int col, char c) {
-	asm ("movl %0, %%edi;"			: :"r"(((80 * row + col) * 2))  :"%edi"); // 写在屏幕的第5行第0列
-	asm ("movw %0, %%eax;"			: :"r"(0x0c00|c) 				:"%eax"); // 0x0黑底,0xc红字,字母ASCII码
-	asm ("movw %%ax, %%gs:(%%edi);" : : 							:"%edi"); // 写入显存
+void video_print(int row, int col, char c) {
+	asm ("movl %0, %%edi;"			: :"r"(((80 * row + col) * 2))  :"%edi");
+	asm ("movw %0, %%eax;"			: :"r"(0x0c00|c) 				:"%eax");
+	asm ("movw %%ax, %%gs:(%%edi);" : : 							:"%edi");
 	while(1);
 }
-
 */
 
 void bootMain(void) {
-	// int a=10, b;
-    // asm ("movl %1, %%eax;movl %%eax, %0;":"=r"(b):"r"(a):"%eax"); /* clobbered register */
-	// asm_print(5, 2, 'x');
 	/* 加载内核至内存，并跳转执行 */
-	// while (1);
 
 	struct ELFHeader *elf;
 	struct ProgramHeader *ph;
@@ -28,17 +23,17 @@ void bootMain(void) {
 	}
 
 	elf = (struct ELFHeader *)buf;
-//	asm_print(5, 0, (elf->entry));
 
 	/* Load each program segment */
 	ph = (struct ProgramHeader *)(buf + elf->phoff);
-	int i;
-	for(i = 0; i < 2; ++i) {
+	for(int i = 0; i < elf->phnum; ++i) {
+
 		/* Scan the program header table, load each segment into memory */
+		if (ph->type == 1) { // LOAD Section
+
 			/* read the content of the segment from the ELF file
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-		//if (ph->type == 1) {
 			unsigned int p = ph->vaddr, q = ph->off;
 			while (q < ph->vaddr + ph->filesz) {
 				*(unsigned char*)p = *(unsigned char*)(buf + q);
@@ -52,15 +47,15 @@ void bootMain(void) {
 				q++;
 				p++;
 			}
-		//}
+		}
 
 		ph++;
 	}
 
+	/* jmp to kernel */
 	void (*entry)(void);
 	entry = (void*)(elf->entry);
 	entry();
-
 }
 
 void waitDisk(void) { // waiting for disk
